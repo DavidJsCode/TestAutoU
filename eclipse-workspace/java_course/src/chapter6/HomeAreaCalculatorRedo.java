beffe54acb1f325ab9fad68c119bafb083925ec6
Jaroneski <djaroneski@vtic.com>
2020-01-20 10:00:34 -0500

    junky commit

--- eclipse-workspace/java_course/src/chapter6/HomeAreaCalculatorRedo.java
+++ eclipse-workspace/java_course/src/chapter6/HomeAreaCalculatorRedo.java
@@ -4,26 +4,33 @@ import java.util.Scanner;
 
 public class HomeAreaCalculatorRedo {
 
-	public static void main(String[] args) {
+    private Scanner scanner = new Scanner(System.in);
 
-		Rectangle kitchen = getRoom();
-		Rectangle bathroom = getRoom();
-		
+    public static void main(String args[]){
 
-	}
+        HomeAreaCalculatorRedo calculator = new HomeAreaCalculatorRedo();
+        Rectangle kitchen = calculator.getRoom();
+        Rectangle bathroom = calculator.getRoom();
 
-	public static Rectangle getRoom() {
-		Scanner scanner = new Scanner(System.in);
-	
-		System.out.println("Enter the width");
-		double width = scanner.nextDouble();
-				
-		System.out.println("Enter the length");
-		double length = scanner.nextDouble();
-		
-		scanner.close();
-		
-		return Rectangle;
-	}
-	
+        double area = calculator.calculateTotalArea(kitchen, bathroom);
+
+        System.out.println("The total area is: " + area);
+
+        calculator.scanner.close();
+    }
+
+    public Rectangle getRoom(){
+
+        System.out.println("Enter the length of your room:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of your room:");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
+    }
+
+    public double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2){
+        return rectangle1.calculateArea() + rectangle2.calculateArea();
+    }
 }
--- eclipse-workspace/java_course_exercises/src/exercises/Ch06CalcPhoneBill.java
+++ eclipse-workspace/java_course_exercises/src/exercises/Ch06CalcPhoneBill.java
@@ -19,10 +19,14 @@
 include three constructors
     a default one 
     one that accepts the ID only
-    one that accepts all fields  
+    one that accepts all fields - id, alloted minutes, minutes used  
 
 No matter which of these constructors you use, all fields should be set eventually.
 Create a different class that instantiates the PhoneBill and prints out an itemized bill.
+
+a method that asks for and returns the monthly fee and minutes used
+
+start with the method that prints the bill, that way you know what you need to get returned to that class?
  */
 
 package exercises;
@@ -37,49 +41,14 @@ public class Ch06CalcPhoneBill {
 		
 	public static void main(String[] args) {	
 	
-		System.out.println("Enter the monthly fee:");
-		double monthlyFee = scanner.nextDouble(); 
-		System.out.println("Enter overage minutes:");
-		double overageMinutes = scanner.nextDouble();
-		scanner.close();
 		
-		double overageFee = calculateOverage(overageMinutes);
-		double totalGross = calculateGross(monthlyFee, overageFee);
-		double taxTotal = calculateTax(totalGross);
-		double total = calculateTotal(taxTotal, totalGross);
-		printBill(monthlyFee, overageFee, taxTotal, total  );
-	}
-		
-		//calc overage fee method
-	public static double calculateOverage(double minutes) {
-		double overageFee = overCharge * minutes;
-		return overageFee;
-	}	
-		
-	public static double calculateGross(double monthlyFee, double overageFee) {
-		double totalGross = overageFee + monthlyFee;
-		return totalGross;
-	}
-
-		//calc tax method
-	public static double calculateTax(double totalGross) {
-		double taxTotal = (totalGross * tax); 
-		return taxTotal;
 	}
 	
-		//calculate total
-	public static double calculateTotal(double taxTotal, double totalGross) {
-		double total = taxTotal + totalGross;
-		return total;
+	public class PhoneBill {
+		//
+		//
+		//
 	}
-		
-		//print total
-	public static void printBill(double monthlyFee, double overageFee, double taxTotal, double total) {
-		System.out.println("Phone Bill Statement");
-		System.out.println("Plan: " + monthlyFee );
-		System.out.println("Overage: " + overageFee );
-		System.out.println("Tax: " + taxTotal);
-		System.out.println("Total: " + total);
-	}
+	
 	
 }

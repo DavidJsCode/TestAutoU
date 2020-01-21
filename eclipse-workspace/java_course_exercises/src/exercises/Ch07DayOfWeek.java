95d7c8122258aa065d69f4572c729080d17c9965
David <codingaddy@gmail.com>
2020-01-20 21:35:42 -0500

    Commit to local, Ch7 Grades and optional exercise from Home PC, 20200120

---
+++ eclipse-workspace/java_course_exercises/src/exercises/Ch07DayOfWeek.java
@@ -0,0 +1,15 @@
+package exercises;
+
+//array contains days of week
+//User will enter a numeric value, 1 through 7
+//User will get the day of week that matches
+//Day 1 = Monday
+//Add a thing to make sure number entered < 8 for kicks
+
+public class Ch07DayOfWeek {
+
+	public static void main(String[] args) {
+
+	}
+
+}

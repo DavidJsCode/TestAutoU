a573a2e3d183100d0d2edaddff00aaafd8b83dc6
Jaroneski <djaroneski@vtic.com>
2020-01-24 17:29:25 -0500

    from vtic on Jan 24 2020

--- eclipse-workspace/java_course_exercises/src/exercises/Ch09PasswordCheck.java
+++ eclipse-workspace/java_course_exercises/src/exercises/Ch09PasswordCheck.java
@@ -13,17 +13,21 @@ public class Ch09PasswordCheck {
 
 	private static String oldPassword = "pass1word";
 	private static String username = "djar";
-	private static Scanner scanner = new Scanner(System.in);
 	private static char[] uppers = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-	private static char[] specialCharacters = {'!','@','#','$','%','&','*',')','(','_','-','=','+','/','?'};
+	private static String[] specialCharacters = {"!","@","#","$","%","&","*",")","(","_","-","=","+","/","?"};
 	
+	private static Scanner scanner = new Scanner(System.in);
+	private static boolean result;
+	
+	// start main
 	public static void main(String[] args) {
 
 	String newPassword = enterPassword();
-	checkUppercase(newPassword);
-	checkSpecialCharacter(newPassword);
-	checkUsername(newPassword);
-	checkAgainstOldPassword(newPassword);
+	scanner.close();
+	if (!checkUppercase(newPassword)) System.out.println("The password must contain a capital letter");
+	//checkSpecialCharacter(newPassword);
+	//checkUsername(newPassword);
+	//checkAgainstOldPassword(newPassword);
 	}
 	
 	private static String enterPassword() {
@@ -34,14 +38,18 @@ public class Ch09PasswordCheck {
 	
 	private static boolean checkUppercase(String text){
 		//if string contains  uppercase, return true
-		for (int i = 0; i < uppers.length; i++) {
-			if (text.contains(uppers[i], 0)) {
-				
+		result = false;
+		for (int i = 0; i < text.length(); i++) {
+			char currentLetter = text.charAt(i);
+	           if(currentLetter == uppers[i]) {
+	                result = true;
+	                break;
+				}
 			}
-		}
-		//else return false, write message
+		return result;		
 	}
 	
+	/*
 	private static boolean checkSpecialCharacter(String newPassword) {
 		
 	}
@@ -53,5 +61,5 @@ public class Ch09PasswordCheck {
 	private static boolean checkAgainstOldPassword(String newPassword) {
 	
 	}
-
-}
+*/
+} 
